9d79b940662196867c55c4d7552139b7fb9c8281
Christopher Rolls <imperialrolls@gmail.com>
2018-02-21 01:55:18 -0700

    parallax

--- js/dream.js
+++ js/dream.js
@@ -2,3 +2,8 @@
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
+
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
