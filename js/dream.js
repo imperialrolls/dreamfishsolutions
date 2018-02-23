014ed0c82451c1077ce57e8a1484083cffeeb7eb
laurentbaig <ljb0904@gmail.com>
2018-02-22 20:22:40 -0700

    Added contact page with supporting php and js

--- js/dream.js
+++ js/dream.js
@@ -5,5 +5,25 @@ $(".button-collapse").sideNav();
 
 
 $(document).ready(function(){
-  $('.parallax').parallax();
+    //$('.parallax').parallax();
+
+    $("#contact-submit").click(function () {
+        let form = {
+            name: $("input[name=name]").val(),
+            email: $("input[name=email]").val(),
+            message: $("textarea[name=message]").val(),
+            recaptcha: $("input[name=captcha]").val()
+        };
+
+        $.ajax({
+            url: 'php/contact_verify.php',
+            data: form,
+            type: 'post',
+            success: function(result) {
+                let res = JSON.parse(result);
+                Materialize.toast(res['result'][0], 4000);
+            }
+        });
+    });
+
 });
